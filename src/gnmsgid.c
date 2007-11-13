a19e015d2c15bf25209fa2f0f6de4f9d60906dfe
Nick Voronin <nick_voronin@users.sourceforge.net>
2007-11-13 11:07:09 +0000

    fix size specification in printf for 64-bit architecture

--- src/gnmsgid.c
+++ src/gnmsgid.c
@@ -248,7 +248,7 @@ int main(int argc, char *argv[])
             msgid = GenMsgIdEx(seqdir, seqoutrun, NULL, &msgiderr);
             if (msgiderr)
             fprintf(stderr, "warning (id #%i): %s, fall to ugly old algorythm\n", i, msgiderr);
-            printf("%08lx\n", msgid);
+            printf("%08x\n", msgid);
         }
     }
     return -1;
