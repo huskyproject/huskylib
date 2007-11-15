f8d351842385dd9ab3cd64f6deb31589669693fc
Nick Voronin <nick_voronin@users.sourceforge.net>
2007-11-15 04:36:45 +0000

    warning-less unused() macro (patch from Alexander Shiyan)

--- huskylib/unused.h
+++ huskylib/unused.h
@@ -40,7 +40,7 @@
 #if defined(_MSC_VER)
 #define unused(x) ((void)(x))
 #elif defined(PACIFIC) || defined(__GNUC__)
-#define unused(x)
+#define unused(x) while(0){if(x){};}
 #elif defined(__HIGHC__)
 #define unused(x) ((x) = (x))
 #else
