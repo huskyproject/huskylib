e10aa083e628aeab8812efaaf52fb52e33ba9e00
Nick Voronin <nick_voronin@users.sourceforge.net>
2010-03-19 00:37:17 +0000

    Just for the sake of integrity, FreeBSD and  X86_64 are not mutually exclusive, which leads to type redefinition.

--- huskylib/typesize.h
+++ huskylib/typesize.h
@@ -108,7 +108,7 @@ typedef          void       VOID;
 #endif
 #endif                                          /*  #if defined(__EMX__) */
 
-#if (defined(__linux__) && !(defined(__alpha) || defined(__X86_64__))) || defined(__FreeBSD__) || defined(__NetBSD__)|| defined(__DJGPP__) || defined(__BEOS__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__QNXNTO__)
+#if (defined(__linux__) && !(defined(__alpha) || defined(__X86_64__))) || (defined(__FreeBSD__)  && !(defined(__alpha) || defined(__X86_64__))) || defined(__NetBSD__)|| defined(__DJGPP__) || defined(__BEOS__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__QNXNTO__)
 typedef   signed char       CHAR;               /*  1 byte */
 typedef unsigned char      UCHAR;               /*  1 byte */
 typedef   signed int        INT32;              /*  4 byte */
