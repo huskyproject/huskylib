fe2c98b719a60dc3a44955bf1a7468ccd4fdbb60
Michael Dukelsky <dukelsky@users.noreply.github.com>
2021-02-02 18:46:11 +0300

    check tmdate for nullptr at the very beginning

--- src/cvtdate.c
+++ src/cvtdate.c
@@ -74,6 +74,11 @@ static void near InitCvt(void)
 /* Convert a DOS-style bitmapped date into a 'struct tm'-type date. */
 struct tm * _fast DosDate_to_TmDate(union stamp_combo * dosdate, struct tm * tmdate)
 {
+    if (tmdate == NULL)
+    {
+        return tmdate;
+    }
+
     if(is_dst == -1)
     {
         InitCvt();
@@ -90,16 +95,13 @@ struct tm * _fast DosDate_to_TmDate(union stamp_combo * dosdate, struct tm * tmd
             return tmdate;
         }
 
-        if(tmdate)
-        {
-            tmdate->tm_mday  = dosdate->msg_st.date.da;
-            tmdate->tm_mon   = dosdate->msg_st.date.mo - 1;
-            tmdate->tm_year  = dosdate->msg_st.date.yr + 80;
-            tmdate->tm_hour  = dosdate->msg_st.time.hh;
-            tmdate->tm_min   = dosdate->msg_st.time.mm;
-            tmdate->tm_sec   = dosdate->msg_st.time.ss << 1;
-            tmdate->tm_isdst = is_dst;
-        }
+        tmdate->tm_mday  = dosdate->msg_st.date.da;
+        tmdate->tm_mon   = dosdate->msg_st.date.mo - 1;
+        tmdate->tm_year  = dosdate->msg_st.date.yr + 80;
+        tmdate->tm_hour  = dosdate->msg_st.time.hh;
+        tmdate->tm_min   = dosdate->msg_st.time.mm;
+        tmdate->tm_sec   = dosdate->msg_st.time.ss << 1;
+        tmdate->tm_isdst = is_dst;
     }
 
     return tmdate;
